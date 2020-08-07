f89b080c696ee8dd3723b9241293c91f94cc7787
Himanshu <himanshu.t@e2logy.com>
2020-08-07 10:04:50 +0530

    - Demo Commit

--- BotProject/BotProject/BookingDetails.cs
+++ BotProject/BotProject/BookingDetails.cs
@@ -8,7 +8,7 @@ namespace BotProject
     public class BookingDetails
     {
         public string Destination { get; set; }
-
+         
         public string Origin { get; set; }
 
         public string TravelDate { get; set; }
